1f6484593e519011264a7a02ad873b879f177051
AsadUllahShaikh123 <asadullahshaikh713@gmail.com>
2021-11-16 03:34:12 -0800

    Implemented Header file

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header" style={{textAlign:'center',fontSize:'4rem',fontFamily:'sans-serif',color:'white'}}>
+            INITUITIVE QUIZ APP 
+        </div>
+    )
+}
+
+export default Header

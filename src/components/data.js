cd296a4cec7dae73a99283e11e54b559fc331edd
AsadUllahShaikh123 <asadullahshaikh713@gmail.com>
2021-11-16 03:33:13 -0800

    extracting data

---
+++ src/components/data.js
@@ -0,0 +1,15 @@
+
+export let data = [
+    {
+        category: 'Science',
+        value: 1
+    },
+    {
+        category: 'Computer',
+        value: 2
+    },
+    {
+        category: 'Socialogy',
+        value: 3
+    }
+]
